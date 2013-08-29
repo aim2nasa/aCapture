569fca01900295e89a492ded7943fbd864d76414
skwak <skwak@e51b8d86-e0d3-1b4d-a83c-86e408438812>
2013-08-29 17:56:06 +0000

    comInit 재수정 System Device Enumerator 구하는 테스트 케이스 추가

--- gUnitTest/deviceTest.cpp
+++ gUnitTest/deviceTest.cpp
@@ -1,21 +1,27 @@
 #include <gtest/gtest.h>
 #include <objbase.h>
-
-void comInit()
-{
-	HRESULT hr = CoInitialize(NULL);
-	EXPECT_EQ(hr,S_OK);
-	hr = CoInitialize(NULL);
-	EXPECT_EQ(hr,S_FALSE);
-}
-
-void comUninit()
-{
-	CoUninitialize();
-}
+#include <atlbase.h>
+#include <strmif.h>
+#include <uuids.h>
 
 TEST(comInit,deviceTest)
 {
-	comInit();
-	comUninit();
+	HRESULT hr = CoInitialize(NULL);
+	if(hr==S_OK) {
+		hr = CoInitialize(NULL);
+		EXPECT_EQ(hr,S_FALSE);
+	}
+	CoUninitialize();
+}
+
+TEST(devEnum,deviceTest)
+{
+	CoInitialize(NULL);
+
+	CComPtr<ICreateDevEnum> pDeviceEnum;
+	HRESULT hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER,IID_ICreateDevEnum, (void **)&pDeviceEnum);
+	if(FAILED(hr)) 
+		FAIL();
+
+	CoUninitialize();
 }

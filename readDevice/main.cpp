270ae0aacb3aeb240ac026cab422fdc05f9c3de2
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2013-09-04 13:48:40 +0000

    장치 이름을 뿌려주는 구현 추가

--- readDevice/main.cpp
+++ readDevice/main.cpp
@@ -7,14 +7,21 @@ int main(int argc, char* argv[])
 
 	CoInitialize(NULL);
 
-	{
-		CDxDev dev;
-		HRESULT hr = dev.devRead(CLSID_AudioInputDeviceCategory);
-		if(FAILED(hr)){
-			std::wcout<<L"Error: "<<dev.errorMsg()<<std::endl;
-			exit(1);
-		}
+	CDxDev* pDev = new CDxDev();
+	HRESULT hr = pDev->devRead(CLSID_AudioInputDeviceCategory);
+	if(FAILED(hr)){
+		std::wcout<<L"Error: "<<pDev->errorMsg()<<std::endl;
+		exit(1);
 	}
+
+	std::list<CName> names = pDev->names();
+	std::wcout<<"Devices :"<<names.size()<<std::endl;
+
+	for(std::list<CName>::iterator it=names.begin();it!=names.end();it++){
+		std::wcout<<"\tname:"<<(*it).m_friendly<<std::endl;
+	}
+
+	delete pDev;
 
 	CoUninitialize();
 

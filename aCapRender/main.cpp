15e74f64feceac2a85e8e69c66d9bd3580c3cca2
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2013-10-01 17:25:32 +0000

    출력 메세지 내용 변경

--- aCapRender/main.cpp
+++ aCapRender/main.cpp
@@ -18,7 +18,7 @@ int main (void)
 	String deviceName1(_T("마이크(Realtek High Definition Aud"));
 
 	if(pDxDev->add(CLSID_AudioInputDeviceCategory,deviceName1)) {
-		wcout<<"<"<<deviceName1<<"> added"<<endl;
+		wcout<<"Filter <"<<deviceName1<<"> added"<<endl;
 	}else{
 		wcout<<"<"<<deviceName1<<"> add failed :"<<pDxDev->errorMsg()<<endl;
 		return 1;
@@ -28,15 +28,18 @@ int main (void)
 	String deviceName2(_T("스피커(Realtek High Definition Aud"));
 
 	if(pDxDev->add(CLSID_AudioRendererCategory,deviceName2)) {
-		wcout<<"<"<<deviceName2<<"> added"<<endl;
+		wcout<<"Filter <"<<deviceName2<<"> added"<<endl;
 	}else{
 		wcout<<"<"<<deviceName2<<"> add failed :"<<pDxDev->errorMsg()<<endl;
 		return 1;
 	}
 
 	//Connect input to output
-	if(pDxDev->connect(deviceName1,String(_T("Capture")),deviceName2,String(_T("Audio Input pin (rendered)")))){
-		wcout<<"Two filters are connected"<<endl;
+	String pin1(_T("Capture")),pin2(_T("Audio Input pin (rendered)"));
+	if(pDxDev->connect(deviceName1,pin1,deviceName2,pin2)){
+		wcout<<"following filters are connected "<<endl;
+		wcout<<"\t("<<deviceName1<<","<<pin1<<") "<<endl;
+		wcout<<"\t("<<deviceName2<<","<<pin2<<") "<<endl;
 	}else{
 		wcout<<"failed to connect filters : "<<pDxDev->errorMsg()<<endl;
 		return 1;
@@ -44,9 +47,9 @@ int main (void)
 
 	//Now run the graph
 	if(pDxDev->run()) {
-		wcout<<_T("now running")<<endl;
+		wcout<<_T("Now running...")<<endl;
 	}else{
-		wcout<<_T("run failed,")<<pDxDev->errorMsg()<<endl;
+		wcout<<_T("failed to run :")<<pDxDev->errorMsg()<<endl;
 		return 1;
 	}
 

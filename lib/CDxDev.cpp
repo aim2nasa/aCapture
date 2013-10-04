cfdba0c7e6dd138e6a88d3fde8551ed29e926106
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2013-10-04 19:28:53 +0000

    CDxDev std::list<CName> m_names; 자료 구조 삭제 및 이에 따른 변경

--- lib/CDxDev.cpp
+++ lib/CDxDev.cpp
@@ -64,7 +64,6 @@ bool CDxDev::devRead(REFCLSID clsidDeviceClass)
 	CComPtr <IEnumMoniker> pEnumCat;
 	VARIANT varName;
 
-	m_names.clear();
 	HRESULT hr = m_pDeviceEnum->CreateClassEnumerator(clsidDeviceClass, &pEnumCat, 0);// Enumerate the specified device, distinguished by DEVICE_CLSID
 	if(hr == S_OK) {
 		IMoniker *pDeviceMonik = NULL;
@@ -90,7 +89,6 @@ bool CDxDev::devRead(REFCLSID clsidDeviceClass)
 				}
 				VariantClear(&varName);
 
-				m_names.push_back(name);
 				names.push_back(name);
 			}else {
 				hrFailed(hr);
